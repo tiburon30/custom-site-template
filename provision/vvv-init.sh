f9e8e8b33643b8e373826b821d2908cc71cdd531
humandevmode <humandevmode@gmail.com>
2018-03-19 13:08:11 +0300

    Remove .git

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -9,6 +9,8 @@ WP_TYPE=`get_config_value 'wp_type' "single"`
 DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
 DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
 
+rm -rf .git .gitignore
+
 # Make a database, if we don't already have one
 echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
@@ -45,14 +47,7 @@ if ! $(noroot wp core is-installed); then
   fi
 
   noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.loc" --admin_password="password"
-  noroot wp plugin install query-monitor --activate
-  noroot wp plugin install wordpress-seo --activate
-  noroot wp plugin install breadcrumb-navxt --activate
-  noroot wp plugin install insert-headers-and-footers --activate
-  noroot wp plugin install simple-301-redirects --activate
-  noroot wp plugin install contact-form-7 --activate
-  noroot wp plugin install flamingo --activate
-  noroot wp plugin install cyr3lat --activate
+  noroot wp plugin install query-monitor wordpress-seo breadcrumb-navxt cyr3lat contact-form-7 flamingo --activate
 
   noroot wp plugin install https://github.com/humandevmode/wp-core-plugin/archive/master.zip
   noroot composer install -o -d ${VVV_PATH_TO_SITE}/public_html/wp-content/plugins/wp-core-plugin
